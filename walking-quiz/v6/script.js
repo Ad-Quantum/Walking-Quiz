e7723c611397713a38d6eee7f99ea12a9a168c86
ADQ <112559898+Ad-Quantum@users.noreply.github.com>
2026-02-24 18:49:26 +0300

    40del

--- walking-quiz/v6/script.js
+++ walking-quiz/v6/script.js
@@ -410,7 +410,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clearInterval(timer);
         setTimeout(() => showView(1), 500);
       }
-    }, 35);
+    }, 40);
   }
 
   document.body.addEventListener("click", (e) => {
